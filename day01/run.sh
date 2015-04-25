16afdc0508cdb61edeb2e5dd33a2d648ae611437
alvie <alvie.zhang@gmail.com>
2015-04-25 10:08:52 +0800

    update execution script

--- day01/run.sh
+++ day01/run.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-..\z_tools\nask.exe helloos.nas helloos.img
-..\z_tools\imgtol.com w a: helloos.img
-copy helloos.img ..\z_tools\qemu\fdimage0.bin
-..\z_tools\make.exe -C ../z_tools/qemu
+nasm helloos.nas -o helloos.img
+qemu-system-i386 -fda helloos.img
